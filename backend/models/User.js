737e6d01bf010811a8d1a4bb7bee12d2a0169b0c
ismailbaoud <ibaoud46@gmail.com>
2025-04-23 11:30:57 +0100

    update le model user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,53 +1,41 @@
+const { DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
         username: {
             type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
+            allowNull: false
         },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false
         },
         role: {
-            type: DataTypes.ENUM('user', 'admin'),
+            type: DataTypes.STRING,
             defaultValue: 'user'
         }
     }, {
-        timestamps: true,
         hooks: {
             beforeCreate: async (user) => {
-                if (user.password) {
-                    const salt = await bcrypt.genSalt(10);
-                    user.password = await bcrypt.hash(user.password, salt);
-                }
+                const hashed = await bcrypt.hash(user.password, 10);
+                user.password = hashed;
             },
             beforeUpdate: async (user) => {
                 if (user.changed('password')) {
-                    const salt = await bcrypt.genSalt(10);
-                    user.password = await bcrypt.hash(user.password, salt);
+                    user.password = await bcrypt.hash(user.password, 10);
                 }
             }
         }
     });
 
-    User.prototype.checkPassword = async function (password) {
-        return await bcrypt.compare(password, this.password);
+    User.prototype.checkPassword = function (password) {
+        return bcrypt.compare(password, this.password);
     };
 
     return User;
